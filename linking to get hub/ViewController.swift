6dfb1650da9debe889147030cc8130c3eb32f091
Rex <rex@georges-macbook-pro.local>
2016-04-10 20:19:30 -0400

    Initial Commit

---
+++ linking to get hub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  linking to get hub
+//
+//  Created by Rex on 4/10/16.
+//  Copyright © 2016 Rex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ linking to get hubTests/linking_to_get_hubTests.swift
@@ -0,0 +1,36 @@
+//
+//  linking_to_get_hubTests.swift
+//  linking to get hubTests
+//
+//  Created by Rex on 4/10/16.
+//  Copyright © 2016 Rex. All rights reserved.
+//
+
+import XCTest
+@testable import linking_to_get_hub
+
+class linking_to_get_hubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

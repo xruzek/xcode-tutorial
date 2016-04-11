b99aa660d23fbe6afb1035ac0e6582081c0cdb3e
Rex <rex@georges-macbook-pro.local>
2016-04-10 20:25:31 -0400

    I added a comment

--- linking to get hub/ViewController.swift
+++ linking to get hub/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // I added a comment
     }
 
     override func didReceiveMemoryWarning() {
